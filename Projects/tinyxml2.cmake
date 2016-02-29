e1e75d4bb5b6584a31c03d9c85471359741b665c
nenetto <nenetto@gmail.com>
2016-02-29 13:20:50 +0100

    delete external xml

--- Projects/tinyxml2.cmake
+++ Projects/tinyxml2.cmake
@@ -4,15 +4,6 @@
 set(EP_OPTION_NAME "USE_${EP_NAME}")
 set(EP_REQUIRED_PROJECTS Git)
 set(EP_URL "https://github.com/leethomason/tinyxml2.git")
-
-cma_end_definition()
-# -----------------------------------------------------------------------------
-
-
-
-set(EP_OPTION_NAME "USE_${EP_NAME}")
-set(EP_REQUIRED_PROJECTS Git)
-set(EP_URL "https://github.com/leethomason/tinyxml2.git")
 set(EP_OPTION_DESCRIPTION "${EP_NAME} Project")
 set(EP_OPTION_DEFAULT OFF)
 
@@ -22,26 +13,39 @@ cma_end_definition()
 # -----------------------------------------------------------------------------
 
 
+set(EP_CMAKE_ARGS
+-DBUILD_SHARED_LIBS:BOOL=OFF
+-DBUILD_STATIC_LIBS:BOOL=ON
+-DBUILD_TESTING:BOOL=OFF
+-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+-DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
+-DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
+-DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_BINARY_DIR}/${EP_NAME}-install
+)
 
 
-if(${PROJECT_NAME}_tinyxml2_EXTERNAL_DIR)
+ExternalProject_Add(${EP_NAME}
+DEPENDS ${EP_REQUIRED_PROJECTS}
+# download
+GIT_REPOSITORY ${EP_URL}
+GIT_TAG 2.2.0
+# patch
+# update
+UPDATE_COMMAND ""
+# configure
+SOURCE_DIR ${PROJECT_BINARY_DIR}/${EP_NAME}
+CMAKE_ARGS ${EP_CMAKE_ARGS}
+# build
+BINARY_DIR ${PROJECT_BINARY_DIR}/${EP_NAME}-build
+# install
+# install
+INSTALL_DIR ${${PROJECT_NAME}_INSTALL_DIR}
+INSTALL_COMMAND
+# test
+)
 
-    ExternalProject_Add(${EP_NAME}
-       DOWNLOAD_COMMAND ""
-       CONFIGURE_COMMAND ""
-       BUILD_COMMAND ""
-       INSTALL_COMMAND "")
-
-  set(${PROJECT_NAME}_${EP_NAME}_DIR ${${PROJECT_NAME}_tinyxml2_EXTERNAL_DIR} CACHE INTERNAL "")
-
-else()
-    set(EP_CMAKE_ARGS
-    -DBUILD_SHARED_LIBS:BOOL=OFF
-    -DBUILD_STATIC_LIBS:BOOL=ON
-    -DBUILD_TESTING:BOOL=OFF
-    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-    -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
-    -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
+set(${PROJECT_NAME}_${EP_NAME}_DIR ${PROJECT_BINARY_DIR}/${EP_NAME}-install CACHE INTERNAL "")
+S}
     -DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_BINARY_DIR}/${EP_NAME}-install
     )
 
