c4e83ae2a08199cb9960a18835ad7d7a4b4309bd
nenetto <nenetto@gmail.com>
2016-09-26 13:45:42 +0200

    Delete old projects and create new clean version using new API

--- CalibrationValidation/inc/CalibrationValidation.h
+++
@@ -1,8 +0,0 @@
-// This is an example for a heaer file
-// It defines the square of a number function
-
-#include "CalibrationValidationConfig.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
